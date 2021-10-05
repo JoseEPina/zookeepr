cc22173d7d26c07f205b53f19090fc68bd25917e
JoseEPina <jose.edpina@gmail.com>
2021-10-04 20:59:50 -0600

    add Heroku

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const { animals } = require('./data/animals');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 function filterByQuery(query, animalsArray) {
    let personalityTraitsArray = [];
    // Note that we save the animalsArray as filteredResults here:
    let filteredResults = animalsArray;
-   
+
    if (query.personalityTraits) {
       // Save personalityTraits as a dedicated array.
       // If personalityTraits is a string, place it into a new array and save.
@@ -49,6 +51,6 @@ app.get('/api/animals', (req, res) => {
    res.json(results);
 });
 
-app.listen(3001, () => {
-   console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+   console.log(`API server now on port ${PORT}!`);
 });
